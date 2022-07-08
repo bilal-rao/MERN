8a411961811faebd12786caed9bd3327eca74023
Bilal Saleem <bilal.saleem.1877@ivacy.com>
2022-07-09 01:01:44 +0500

    userlist and user item components created

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,16 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <div>users</div>;
+  const USERS = [
+    {
+      id: 1,
+      name: "Bilal",
+      image: "https://randomuser.me/api/portraits/men/62.jpg",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
