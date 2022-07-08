5fd5efd88aadfc41f006cc8576bb46ac12cb1425
Bilal Saleem <bilal.saleem.1877@ivacy.com>
2022-07-08 18:55:04 +0500

    users and places routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,3 @@
-// import React from'react';
-// import ReactDOM from 'react-dom';
-
-// import App from './App.js';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
