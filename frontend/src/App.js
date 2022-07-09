ea7bc20d7548c4611ea6bf99cab9335f5d426a0d
Bilal Saleem <bilal.saleem.1877@ivacy.com>
2022-07-09 16:31:55 +0500

    added main header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,19 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 //Pages
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Users />} />
-        <Route path="/places/new" element={<NewPlace />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/places/new" element={<NewPlace />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 };
